8d709ae3e3ad96f62489d7e04c0bffd66a02e4b9
ShionKigami <steblewets.ant@gmail.com>
2026-06-01 11:54:30 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,126 +1,275 @@
 <?php
+header('Content-Type: text/html; charset=UTF-8');
 
+$user = 'u82624';
+$pass = '8440989';
+$db = new PDO('mysql:host=localhost;dbname=u82624', $user, $pass, [
+    PDO::ATTR_PERSISTENT => true,
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+]);
 
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    $messages = [];
+    $errors = [];
+    $values = [];
 
-global $db;
-
-$db = new PDO('mysql:host=' . conf('db_host') . ';dbname=' . conf('db_name'), conf('db_user'), conf('db_psw'),
-  array(PDO::MYSQL_ATTR_FOUND_ROWS => true, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
-
-// $db = new PDO('mysql:host=localhost;dbname=u82197', 'u82197', '6410666',
-//   array(PDO::MYSQL_ATTR_FOUND_ROWS => true, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
-
-function db_row($stmt) {
-  return $stmt->fetch(PDO::FETCH_ASSOC);
-}
-
-function db_error() {
-  global $db;
-  return $db->errorInfo();
-}
-
-function db_query($query) {
-  global $db;
-  $q = $db->prepare($query);
-  $args = func_get_args();
-  array_shift($args);
-  $res = $q->execute($args);
-  if ($res) {
-    while ($row = db_row($res)) {
-      if (isset($row['id']) && !isset($r[$row['id']])) {
-        $r[$row['id']] = $row;
-      }
-      else {
-        $r[] = $row;
-      }
+    if (!empty($_COOKIE['save'])) {
+        $messages[] = 'Результаты сохранены.';
+        if (!empty($_COOKIE['login']) && !empty($_COOKIE['pass'])) {
+            $messages[] = sprintf(
+                'Можно <a href="login.php">войти</a> с логином <strong>%s</strong> и паролем <strong>%s</strong>',
+                strip_tags($_COOKIE['login']),
+                strip_tags($_COOKIE['pass'])
+            );
+        }
+        
+        setcookie('save', '', 100000);
+        setcookie('login', '', 100000);
+        setcookie('pass', '', 100000);
     }
-  }
-  return $r;
-}
 
-function db_result($query) {
-  global $db;
-  $q = $db->prepare($query);
-  $args = func_get_args();
-  array_shift($args);
-  $res = $q->execute($args);
-  if ($res) {
-    if ($row = db_row($res)) {
-      return $row[0];
+    $error_fields = ['name', 'phone', 'email', 'birthdate', 'sex', 'languages', 'contract'];
+    foreach ($error_fields as $field) {
+        $errors[$field] = !empty($_COOKIE[$field . '_error']);
+        if ($errors[$field]) {
+            setcookie($field . '_error', '', 100000);
+            $messages[] = '<div class="error">' . getErrorMessage($field) . '</div>';
+        }
+        
     }
-    else {
-      return FALSE;
+
+    $values['name'] = empty($_COOKIE['name_value']) ? '' : strip_tags($_COOKIE['name_value']);
+    $values['phone'] = empty($_COOKIE['phone_value']) ? '' : strip_tags($_COOKIE['phone_value']);
+    $values['email'] = empty($_COOKIE['email_value']) ? '' : strip_tags($_COOKIE['email_value']);
+    $values['birthdate'] = empty($_COOKIE['birthdate_value']) ? '' : strip_tags($_COOKIE['birthdate_value']);
+    $values['sex'] = empty($_COOKIE['sex_value']) ? '' : strip_tags($_COOKIE['sex_value']);
+    $values['languages'] = empty($_COOKIE['languages_value']) ? [] : explode('|', strip_tags($_COOKIE['languages_value']));
+    $values['contract'] = empty($_COOKIE['contract_value']) ? '' : strip_tags($_COOKIE['contract_value']);
+
+    if (empty($errors) && !empty($_COOKIE[session_name()]) && session_start() && !empty($_SESSION['login'])) {
+   
+        try {
+            $stmt = $db->prepare("SELECT id, name, phone, email, birthdate, sex, biography FROM users WHERE login = ?");
+            $stmt->execute([$_SESSION['login']]);
+            $userData = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if ($userData) {
+                $_SESSION['uid'] = $userData['id']; // Убедимся, что uid установлен
+                $values['name'] = strip_tags($userData['name']);
+                $values['phone'] = strip_tags($userData['phone']);
+                $values['email'] = strip_tags($userData['email']);
+                $values['birthdate'] = strip_tags($userData['birthdate']);
+                $values['sex'] = strip_tags($userData['sex']);
+                $values['contract'] = '1'; 
+
+                $lang_stmt = $db->prepare("SELECT language FROM user_languages WHERE user_id = ?");
+                $lang_stmt->execute([$userData['id']]);
+                $values['languages'] = $lang_stmt->fetchAll(PDO::FETCH_COLUMN);
+            }
+        } catch (PDOException $e) {
+            $messages[] = '<div class="error">Ошибка загрузки данных: ' . $e->getMessage() . '</div>';
+        }
     }
-  }
-  else {
-    return FALSE;
-  }
+
+    setcookie('name_value', '', 100000);
+    setcookie('phone_value', '', 100000);
+    setcookie('email_value', '', 100000);
+    setcookie('birthdate_value', '', 100000);
+    setcookie('sex_value', '', 100000);
+    setcookie('languages_value', '', 100000);
+    setcookie('contract_value', '', 100000);
+
+    include('form.php');
+    exit();
 }
+else {
+    $errors = FALSE;
 
-function db_command($query) {
-  global $db;
-  $q = $db->prepare($query);
-  $args = func_get_args();
-  array_shift($args);
-  return $res = $q->execute($args);
-}
+    // ВАЖНО: Сессию нужно стартовать ДО использования $_SESSION
+    session_start();
 
-function db_insert_id() {
-  global $db;
-  return $db->lastInsertId();
-}
-
-function db_get($name, $default = FALSE) {
-  if (strlen($name) == 0) {
-    return $default;
-  }
-  $value = db_result("SELECT value FROM variable WHERE name = ?", $name);
-  if ($value === FALSE) {
-    return $default;
-  }
-  else {
-    return $value;
-  }
-}
-
-function db_set($name, $value) {
-  if (strlen($name) == 0) {
-    return;
-  }
-
-  $v = db_get($name);
-  if ($v === FALSE) {
-    $q = "INSERT INTO variable VALUES (?, ?)";
-    return db_command($q, $name, $value) > 0;
-  }
-  else {
-    $q = "UPDATE variable SET value = ? WHERE name = ?";
-    return db_command($q, $value, $name) > 0;
-  }
-}
-
-function db_sort_sql() {
-}
-
-function db_pager_query() {
-}
-
-function db_array() {
-  $args = func_get_args();
-  $key = array_shift($args);
-  $query = array_shift($args);
-  $q = $db->prepare($query);
-  $res = $q->execute($args);
-  $r = array();
-  if ($res) {
-    while ($row = db_row($res)) {
-      if (!empty($key) && isset($row[$key]) && !isset($r[$row[$key]])) {
-        $r[$row[$key]] = $row;
-      }
-      else {
-        $r[] = $row;
-      }
+    if (empty($_POST['name'])) {
+        setcookie('name_error', '1', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    } elseif (strlen($_POST['name']) > 150) {
+        setcookie('name_error', '1', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    } elseif (!preg_match('/^[a-zA-Zа-яА-ЯёЁ\s]+$/u', $_POST['name'])) {
+        setcookie('name_error', '1', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    } else {
+        setcookie('name_value', $_POST['name'], time() + 30 * 24 * 60 * 60);
     }
-  }
-  return $r;
+
+    if (!empty($_POST['phone']) && !preg_match('/^[\+0-9\s\-\(\)]{10,20}$/', $_POST['phone'])) {
+        setcookie('phone_error', '1', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    } else {
+        setcookie('phone_value', $_POST['phone'], time() + 30 * 24 * 60 * 60);
+    }
+
+    if (!empty($_POST['email']) && !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+        setcookie('email_error', '1', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    } else {
+        setcookie('email_value', $_POST['email'], time() + 30 * 24 * 60 * 60);
+    }
+
+    if (!empty($_POST['birthdate'])) {
+        $date = DateTime::createFromFormat('Y-m-d', $_POST['birthdate']);
+        if (!$date || $date->format('Y-m-d') !== $_POST['birthdate']) {
+            setcookie('birthdate_error', '1', time() + 24 * 60 * 60);
+            $errors = TRUE;
+        } else {
+            setcookie('birthdate_value', $_POST['birthdate'], time() + 30 * 24 * 60 * 60);
+        }
+    }
+
+    $allowed_genders = ['male', 'female'];
+    if (empty($_POST['sex'])) {
+        setcookie('sex_error', '1', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    } elseif (!in_array($_POST['sex'], $allowed_genders)) {
+        setcookie('sex_error', '1', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    } else {
+        setcookie('sex_value', $_POST['sex'], time() + 30 * 24 * 60 * 60);
+    }
+
+    $allowed_languages = ['Pascal', 'C', 'C++', 'JavaScript', 'PHP', 'Python', 'Java', 'Haskel', 'Clojure', 'Prolog', 'Scala', 'Go'];
+    if (empty($_POST['languages'])) {
+        setcookie('languages_error', '1', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    } else {
+        $lang_valid = TRUE;
+        foreach ($_POST['languages'] as $lang) {
+            if (!in_array($lang, $allowed_languages)) {
+                $lang_valid = FALSE;
+                break;
+            }
+        }
+        if (!$lang_valid) {
+            setcookie('languages_error', '1', time() + 24 * 60 * 60);
+            $errors = TRUE;
+        } else {
+            setcookie('languages_value', implode('|', $_POST['languages']), time() + 30 * 24 * 60 * 60);
+        }
+    }
+
+    if (empty($_POST['contract']) || $_POST['contract'] != '1') {
+        setcookie('contract_error', '1', time() + 24 * 60 * 60);
+        $errors = TRUE;
+    } else {
+        setcookie('contract_value', '1', time() + 30 * 24 * 60 * 60);
+    }
+
+    if ($errors) {
+        header('Location: index.php');
+        exit();
+    }
+
+    $error_cookies = ['name', 'phone', 'email', 'birthdate', 'sex', 'languages', 'contract'];
+    foreach ($error_cookies as $field) {
+        setcookie($field . '_error', '', 100000);
+    }
+
+    $is_update = false;
+    // Проверяем авторизацию
+    if (!empty($_SESSION['login'])) {
+        $is_update = true;
+    }
+
+    try {
+        $db->beginTransaction();
+
+        if ($is_update) {
+            // Сначала проверим, существует ли пользователь
+            $check_stmt = $db->prepare("SELECT id FROM users WHERE login = ?");
+            $check_stmt->execute([$_SESSION['login']]);
+            $existing_user = $check_stmt->fetch(PDO::FETCH_ASSOC);
+            
+            if (!$existing_user) {
+                throw new Exception("Пользователь не найден");
+            }
+            
+            $user_id = $existing_user['id'];
+            
+            $stmt = $db->prepare("UPDATE users SET name = ?, phone = ?, email = ?, birthdate = ?, sex = ?, biography = ? WHERE id = ?");
+            $stmt->execute([
+                $_POST['name'],
+                $_POST['phone'] ?? null,
+                $_POST['email'] ?? null,
+                $_POST['birthdate'] ?? null,
+                $_POST['sex'],
+                $_POST['biography'] ?? null,
+                $user_id
+            ]);
+
+            // Удаляем старые языки
+            $del_stmt = $db->prepare("DELETE FROM user_languages WHERE user_id = ?");
+            $del_stmt->execute([$user_id]);
+
+        } else {
+            $login = 'user_' . rand(1000, 9999) . uniqid();
+            $pass = substr(md5(uniqid(rand(), true)), 0, 8);
+            $pass_hash = md5($pass);
+
+            $stmt = $db->prepare("INSERT INTO users (name, phone, email, birthdate, sex, biography, login, pass_hash) VALUES (?, ?, ?, ?, ?, ?, ?, ?)");
+            $stmt->execute([
+                $_POST['name'],
+                $_POST['phone'] ?? null,
+                $_POST['email'] ?? null,
+                $_POST['birthdate'] ?? null,
+                $_POST['sex'],
+                $_POST['biography'] ?? null,
+                $login,
+                $pass_hash
+            ]);
+
+            $user_id = $db->lastInsertId();
+            
+            // Сохраняем логин и пароль для нового пользователя
+            $_SESSION['login'] = $login;
+            $_SESSION['uid'] = $user_id;
+        }
+
+        // Вставляем языки (теперь user_id точно существует)
+        $lang_stmt = $db->prepare("INSERT INTO user_languages (user_id, language) VALUES (?, ?)");
+        foreach ($_POST['languages'] as $lang) {
+            $lang_stmt->execute([$user_id, $lang]);
+        }
+
+        $db->commit();
+
+        setcookie('save', '1', time() + 30 * 24 * 60 * 60);
+
+        if (!$is_update) {
+            setcookie('login', $login, time() + 30 * 24 * 60 * 60);
+            setcookie('pass', $pass, time() + 30 * 24 * 60 * 60);
+        }
+
+        header('Location: index.php');
+        exit();
+
+    } catch (PDOException $e) {
+        $db->rollBack();
+        die('Ошибка базы данных: ' . $e->getMessage());
+    } catch (Exception $e) {
+        $db->rollBack();
+        die('Ошибка: ' . $e->getMessage());
+    }
 }
+
+function getErrorMessage($field) {
+    $messages = [
+        'name' => 'Введите корректное имя.',
+        'phone' => 'Введите корректный номер телефона.',
+        'email' => 'Введите корректный email.',
+        'birthdate' => 'Введите корректную дату рождения.',
+        'sex' => 'Выберите корректный пол.',
+        'languages' => 'Выберите хотя бы один язык программирования.',
+        'contract' => 'Ознакомьтесь с условиями.'
+    ];
+    return $messages[$field] ?? 'Неизвестная ошибка.';
+}
+?>
