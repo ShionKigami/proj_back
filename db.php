179f4a39b124786cbe8340b51244e6534fe17e76
ShionKigami <steblewets.ant@gmail.com>
2026-06-04 22:00:31 +0300

    Update db.php

--- db.php
+++ db.php
@@ -7,9 +7,6 @@ global $db;
 $db = new PDO('mysql:host=' . conf('db_host') . ';dbname=' . conf('db_name'), conf('db_user'), conf('db_psw'),
   array(PDO::MYSQL_ATTR_FOUND_ROWS => true, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 
-// $db = new PDO('mysql:host=localhost;dbname=u82197', 'u82197', '6410666',
-//   array(PDO::MYSQL_ATTR_FOUND_ROWS => true, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
-
 function db_row($stmt) {
   return $stmt->fetch(PDO::FETCH_ASSOC);
 }
